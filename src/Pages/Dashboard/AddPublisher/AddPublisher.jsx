e8243b7c257ed9a36d460ac4c389bb2e9364ae29
Sohelrana2815 <sohel152302@gmail.com>
2024-12-21 11:03:26 +0600

    successfully created add publisher page and post publisher name and logo

--- src/Pages/Dashboard/AddPublisher/AddPublisher.jsx
+++ src/Pages/Dashboard/AddPublisher/AddPublisher.jsx
@@ -1,8 +1,45 @@
 import { useForm } from "react-hook-form";
+import useAxiosPublic from "../../../Hooks/useAxiosPublic";
+
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const AddPublisher = () => {
+  const axiosPublic = useAxiosPublic();
   const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = async (data) => {
+    try {
+      // upload image on img bb then get url
+      const imageFile = { image: data.publisherImage[0] };
+      const response = await axiosPublic.post(image_hosting_api, imageFile, {
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+      });
+      console.log(response.data);
+
+      if (response.data.success) {
+        const publisherInfo = {
+          publisherName: data.publisherName,
+          publisherImage: response.data.data.display_url,
+        };
+
+        const publisherResponse = await axiosPublic.post(
+          "/publishers",
+          publisherInfo
+        );
+
+        if (publisherResponse.data.insertedId) {
+          alert("posted publisher!");
+        }
+      }
+
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error While Adding Publisher", error);
+    }
+  };
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)} className="card-body">
