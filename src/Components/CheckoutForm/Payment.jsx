47ae78f328b37f3cebfe8870e823a12a3d305271
Sohelrana2815 <sohel152302@gmail.com>
2024-12-24 17:15:35 +0600

    make the payment saparate form

---
+++ src/Components/CheckoutForm/Payment.jsx
@@ -0,0 +1,15 @@
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
+import { loadStripe } from "@stripe/stripe-js";
+const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GATEWAY_PK);
+const Payment = () => {
+  return (
+    <div>
+      <Elements stripe={stripePromise}>
+        <CheckoutForm />
+      </Elements>
+    </div>
+  );
+};
+
+export default Payment;
--- src/Pages/Subscriptions/Subscriptions.jsx
+++ src/Pages/Subscriptions/Subscriptions.jsx
@@ -1,18 +1,10 @@
-import { loadStripe } from "@stripe/stripe-js";
 import checkoutImg from "../../assets/CheckoutImg/payment-information-concept-illustration_114360-2296.jpg";
-import { Elements } from "@stripe/react-stripe-js";
-import CheckoutForm from "../../Components/CheckoutForm/CheckoutForm";
 
 // TODO: create account and get pk
-const stripePromise = loadStripe("pk");
+
 const Subscriptions = () => {
   return (
     <div>
-      <div>
-        <Elements stripe={stripePromise}>
-          <CheckoutForm />
-        </Elements>
-      </div>
       <img src={checkoutImg} alt="" />
     </div>
   );
