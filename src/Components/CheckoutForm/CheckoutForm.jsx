14e914bf22ed1d1b4bd56e71bf67c319e600cce1
Sohelrana2815 <sohel152302@gmail.com>
2024-12-26 07:38:45 +0600

    get the client secret(stripe) from server side

--- src/Components/CheckoutForm/CheckoutForm.jsx
+++ src/Components/CheckoutForm/CheckoutForm.jsx
@@ -1,10 +1,37 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useEffect, useState } from "react";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
 
 const CheckoutForm = ({ subscription }) => {
   console.log(subscription);
+  const axiosPublic = useAxiosPublic();
+
+  const { label, value, price } = subscription;
+
+  const [error, setError] = useState("");
+  const [clientSecret, setClientSecret] = useState("");
 
   const stripe = useStripe();
   const elements = useElements();
+
+  useEffect(() => {
+    if (price && value) {
+      const packageData = {
+        packagePrice: price,
+        expireTime: value,
+      };
+
+      axiosPublic
+        .post("create-payment-intent", { packageData })
+        .then((response) => {
+          console.log("Payment intent created:", response.data.clientSecret);
+          setClientSecret(response.data.clientSecret);
+        })
+        .catch((error) => {
+          console.error("Error creating payment intent:", error);
+        });
+    }
+  }, [axiosPublic, price, value]);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -28,8 +55,10 @@ const CheckoutForm = ({ subscription }) => {
 
     if (error) {
       console.log("[error]", error);
+      setError(error.message);
     } else {
       console.log("[PaymentMethod]", paymentMethod);
+      setError("");
     }
   };
 
@@ -41,9 +70,7 @@ const CheckoutForm = ({ subscription }) => {
       >
         <h2 className="text-2xl font-bold text-center text-blue-700">
           Payment Form for{" "}
-          {subscription
-            ? `${subscription.label} - $${subscription.price}`
-            : "Loading..."}
+          {subscription ? `${label} - $${price}` : "Loading..."}
         </h2>
         <p className="text-center text-gray-600">
           Enter your card details below to proceed with the payment.
@@ -70,10 +97,11 @@ const CheckoutForm = ({ subscription }) => {
         <button
           className="w-full bg-blue-500 text-white py-3 rounded-lg text-lg font-medium hover:bg-blue-600 transition duration-200 disabled:opacity-50"
           type="submit"
-          disabled={!stripe}
+          disabled={!stripe || !clientSecret}
         >
           Pay
         </button>
+        <p className="text-red-600">{error}</p>
       </form>
     </div>
   );
