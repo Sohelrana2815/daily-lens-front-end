d4c196191dab78a8de758ef6e1f983429527e9db
Sohelrana2815 <sohel152302@gmail.com>
2024-12-29 08:26:04 +0600

    user data not be post if already the user email is exist

--- src/Components/SocialLogin/SocialLogin.jsx
+++ src/Components/SocialLogin/SocialLogin.jsx
@@ -1,14 +1,41 @@
 import { FcGoogle } from "react-icons/fc";
 import useAuth from "../../Hooks/useAuth";
 import { FaGithub } from "react-icons/fa";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const SocialLogin = () => {
+  const navigate = useNavigate();
+  const axiosPublic = useAxiosPublic();
   const { googleSignIn, gitHubLogin } = useAuth();
 
   const handleGoogleLogin = () => {
     googleSignIn()
       .then((result) => {
-        console.log(result.user);
+        const userData = {
+          email: result.user.email,
+          name: result.user.displayName,
+          photoURL: result.user.photoURL,
+          subscriptionPeriod: null,
+        };
+
+        // Make a POST request to save the user data
+
+        axiosPublic.post("/users", userData).then((response) => {
+          console.log("User data posted successfully:", response.data);
+          if (response.data.insertedId) {
+            setTimeout(() => {
+              navigate("/");
+            }, 500);
+            Swal.fire({
+              position: "center",
+              icon: "success",
+              title: "Sign up successfully",
+              timer: 6000,
+            });
+          }
+        });
       })
       .catch((error) => {
         console.error(error);
@@ -18,6 +45,29 @@ const SocialLogin = () => {
     gitHubLogin()
       .then((result) => {
         console.log(result.user);
+        const userData = {
+          email: result.user.email,
+          name: result.user.displayName,
+          photoURL: result.user.photoURL,
+          subscriptionPeriod: null,
+        };
+
+        // Make a POST request to save the user data
+
+        axiosPublic.post("/users", userData).then((response) => {
+          console.log("User data posted successfully:", response.data);
+          if (response.data.insertedId) {
+            setTimeout(() => {
+              navigate("/");
+            }, 500);
+            Swal.fire({
+              position: "center",
+              icon: "success",
+              title: "Sign up successfully",
+              timer: 6000,
+            });
+          }
+        });
       })
       .catch((error) => {
         console.error(error);
--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,12 +1,79 @@
+import Swal from "sweetalert2";
+import useAxiosPublic from "../../../Hooks/useAxiosPublic";
 import useUsers from "../../../Hooks/useUsers";
 
 const AllUsers = () => {
-  const { allUsers } = useUsers();
+  const { allUsers, refetch } = useUsers();
+
+  const axiosPublic = useAxiosPublic();
+  const makeUserAdmin = (user) => {
+    console.log(user);
+
+    Swal.fire({
+      title: "Are you sure?",
+      text: `Do you want to make ${user?.name} an admin? This action cannot be undone.`,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const response = await axiosPublic.patch(
+          `/users-make-admin/${user._id}`
+        );
+        if (response.data.modifiedCount > 0) {
+          refetch();
+          Swal.fire({
+            title: "User Promoted!",
+            text: `${user?.name} has been successfully made an admin.`,
+            icon: "success",
+            draggable: true,
+            confirmButtonText: "Okay",
+          });
+        }
+      }
+    });
+  };
 
   return (
     <>
-      <div>
-        <h3>{allUsers.length}</h3>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Profile Picture</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {allUsers.map((user) => (
+              <tr key={user._id} className="hover">
+                <th>{user.displayName}</th>
+                <td>{user.email}</td>
+                <td>
+                  <img src={user.photoURL} className="w-10 lg:w-14" alt="" />
+                </td>
+                <td>
+                  {user.isAdmin ? (
+                    "Admin"
+                  ) : (
+                    <button
+                      onClick={() => makeUserAdmin(user)}
+                      className="btn btn-xs btn-success text-white"
+                    >
+                      Make Admin
+                    </button>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </>
   );
