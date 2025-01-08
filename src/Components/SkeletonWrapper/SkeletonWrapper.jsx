0c47371bbb3a0e9b6a2f7c11866ad6ddfc62dcd2
Sohelrana2815 <sohel152302@gmail.com>
2025-01-08 13:23:11 +0600

    added dedicated component for skeleton loading

---
+++ src/Components/SkeletonWrapper/SkeletonWrapper.jsx
@@ -0,0 +1,6 @@
+import Skeleton from "react-loading-skeleton";
+const SkeletonWrapper = ({ loading, children, width, height }) => {
+  return loading ? <Skeleton width={width} height={height} /> : children;
+};
+
+export default SkeletonWrapper;
--- src/Pages/Home/AllPublishers/AllPublishers.jsx
+++ src/Pages/Home/AllPublishers/AllPublishers.jsx
@@ -1,5 +1,6 @@
 import usePublishers from "../../../Hooks/usePublishers";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import AnimatedComponent from "../../../Components/AnimatedComponent/AnimatedComponent";
 const AllPublishers = () => {
   const { publishers } = usePublishers();
 
@@ -9,34 +10,40 @@ const AllPublishers = () => {
       <div className="max-w-screen-xl mx-auto px-4">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 dark:bg-gray-800 p-6">
           {publishers.map((publisher) => (
-            <div
+            <AnimatedComponent
               key={publisher._id}
-              className="bg-white rounded-lg shadow-lg transition-transform transform hover:scale-105 hover:shadow-2xl hover:bg-gray-200  overflow-hidden dark:bg-slate-900 "
+              animation="fade-out"
             >
-              <div className="relative h-40">
-                <img
-                  src={publisher.publisherImage}
-                  alt={publisher.publisherName}
-                  className="w-full h-full object-cover border-b-2 border-red-600"
-                />
+              <div
+                key={publisher._id}
+                className="bg-white rounded-lg shadow-lg transition-transform transform hover:scale-105 hover:shadow-2xl hover:bg-gray-200  overflow-hidden dark:bg-slate-900 "
+              >
+                <div className="relative h-40">
+                  <img
+                    src={publisher.publisherImage}
+                    alt={publisher.publisherName}
+                    className="w-full h-full object-cover border-b-2 border-red-600"
+                  />
+                </div>
+                <div className="p-6">
+                  <h3 className="text-lg font-semibold text-gray-800 mb-2 dark:text-slate-200">
+                    {publisher.publisherName}
+                  </h3>
+                  <p className="text-sm text-gray-600 dark:text-gray-300">
+                    {publisher.description ||
+                      "Publisher description goes here."}
+                  </p>
+                </div>
+                <div className="p-4 border-t flex justify-between">
+                  <button className="text-indigo-600 font-semibold hover:underline dark:text-indigo-100">
+                    Share
+                  </button>
+                  <button className="text-indigo-600 font-semibold hover:underline dark:text-indigo-100">
+                    Learn More
+                  </button>
+                </div>
               </div>
-              <div className="p-6">
-                <h3 className="text-lg font-semibold text-gray-800 mb-2 dark:text-slate-200">
-                  {publisher.publisherName}
-                </h3>
-                <p className="text-sm text-gray-600 dark:text-gray-300">
-                  {publisher.description || "Publisher description goes here."}
-                </p>
-              </div>
-              <div className="p-4 border-t flex justify-between">
-                <button className="text-indigo-600 font-semibold hover:underline dark:text-indigo-100">
-                  Share
-                </button>
-                <button className="text-indigo-600 font-semibold hover:underline dark:text-indigo-100">
-                  Learn More
-                </button>
-              </div>
-            </div>
+            </AnimatedComponent>
           ))}
         </div>
       </div>
